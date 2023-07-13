4dd61a6ba4ec7e5530f360f4e9b29c00ed79bb2f
vmarialuzm <vmarialuzm@gmail.com>
2023-07-13 18:40:30 -0500

    crud con apiview

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,14 @@
-from rest_framework.routers import DefaultRouter
-from .views import TaskView
+# from rest_framework.routers import DefaultRouter
+# from .views import TaskView
 
-router=DefaultRouter()
+# router=DefaultRouter()
 
-router.register("todo", TaskView, basename="todo")
+# router.register("todo", TaskView, basename="todo")
+
+from django.urls import path
+from .views import TodoView, TodoViewDetail
+
+urlpatterns = [
+    path("todo/", TodoView.as_view(), name="todo"),
+    path("todo/<id>/", TodoViewDetail.as_view(), name="todo-detail"),
+]
